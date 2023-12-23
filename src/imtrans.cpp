0c212bb4ee15711edc6b494655fe9a3c4aa7b0f6
Pathal <Pathal@users.noreply.github.com>
2023-12-23 17:37:44 -0500

    Not broken!

--- src/imtrans.cpp
+++ src/imtrans.cpp
@@ -14,11 +14,11 @@ cv::Mat cvx::matlab::imsharpen(const cv::Mat& inp, double ratio, cv::Size kSize,
 	return dst;
 }
 
-// broken?!?
 void cvx::common::clamp(cv::Mat& inp, cv::Scalar minVal, cv::Scalar maxVal) {
 	cv::min(inp, maxVal, inp);
 	cv::max(inp, minVal, inp);
 }
+
 void cvx::common::clamp(cv::Mat& inp, cv::Mat& dest, cv::Scalar minVal, cv::Scalar maxVal) {
 	cv::min(inp, maxVal, dest);
 	cv::max(dest, minVal, dest);
